8ef1ae1fb69adb6839fd4a9aae5271dc516313b2
AbelBusi <cesarabelbustamante@gmail.com>
2025-04-21 15:58:03 -0500

    Se agrego un efecto scroll al momento de bajar y se crearon varias clases css que sirven para personalizar la index data, ademas se agregaron nuevas imagenes,se dio nuevos estilos y todavia sigo en la index de data. v1.1.0.1

--- Principal/assets/JS/data.js
+++ Principal/assets/JS/data.js
@@ -7,3 +7,20 @@ function activate(e) {
 }
 
 document.addEventListener('click',activate,false);
+
+// Función para detectar si el servicio está en el viewport (pantalla)
+// Función para detectar si el elemento está en el viewport (pantalla)
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target); // Deja de observar cuando ya se mostró
+        }
+    });
+}, {
+    threshold: 0.2 // Solo cuando el 20% del elemento ya entró en la pantalla
+});
+
+document.querySelectorAll('.col-md-4').forEach(item => {
+    observer.observe(item);
+});
